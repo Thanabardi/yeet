a9397f952880a85782ea7e82ed045754964f5a34
Thanabardi <Thanabardi@hotmail.com>
2022-02-14 23:23:50 +0700

    apply templates

--- yeet_frontend/src/components/MainMenu.jsx
+++ yeet_frontend/src/components/MainMenu.jsx
@@ -2,9 +2,16 @@ import React from 'react';
 import { useLocation } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 
+import '../assets/MainMenu.css';
+
 const Menu = () => {
   const location = useLocation()
   // console.log(location.state)
+  const url = "https://actions.google.com/sounds/v1/water/waves_crashing_on_rock_beach.ogg";
+  const audio = new Audio(url);
+  audio.play()
+  // audio.loop = true
+
   return (
     <div>
       <h1>Yeet</h1>
