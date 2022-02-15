ca926a41aec62df61b0b60903e81eb2238426e66
Thanabardi <Thanabardi@hotmail.com>
2022-02-15 22:27:23 +0700

    implement sound system

--- yeet_frontend/src/components/MainMenu.jsx
+++ yeet_frontend/src/components/MainMenu.jsx
@@ -2,25 +2,32 @@ import React from 'react';
 import { useLocation } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 
-import '../assets/MainMenu.css';
+import '../assets/style.css';
+import AudioPlayer from './AudioPlayer'
 
 const Menu = () => {
   const location = useLocation()
   // console.log(location.state)
-  const url = "https://actions.google.com/sounds/v1/water/waves_crashing_on_rock_beach.ogg";
-  const audio = new Audio(url);
-  audio.play()
-  // audio.loop = true
+  // const audio = new Audio("/METATRON _SHIKI.mp3")
+
+  // const start = () => {
+  //   audio.play()
+  // }
 
   return (
-    <div>
+    <div className='main-menu'>
       <h1>Yeet</h1>
       <h1>{location.state}</h1>
+      {/* <img src="logo.PNG" alt="Yeet Logo"></img> */}
+
       <Link to={'/play'} state={'Quick Play'}>Quick Play</Link>
       <Link to={'/play'} state={'Competitive'}>Competitive</Link>
       <Link to={'/rank'} state={'rank'}>Rank</Link>
       <Link to={'/profile'} state={'Profile'}>Profile</Link>
       <Link to={'/auth'} state={'Logout'}>Logout</Link>
+      {/* <button onClick={start}>Play</button> */}
+      <AudioPlayer audioPath={"/METATRON _SHIKI.mp3"}/>
+      {/* <AudioPlayer audioPath={"https://actions.google.com/sounds/v1/water/waves_crashing_on_rock_beach.ogg"}/> */}
     </div>
   );
 }
