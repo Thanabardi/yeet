40183c602b6128527a64eba7e05526ea68d12aa0
natchanon-space <natchanon.space@gmail.com>
2022-02-15 22:03:57 +0700

    test username is root but I dont know why so I test it out

--- yeet_backend/api/urls.py
+++ yeet_backend/api/urls.py
@@ -7,10 +7,8 @@ urlpatterns = [
     path("login/", Login.as_view(), name="login"),
     path("logout/", Logout.as_view(), name="logout"),
     path("register/", Register.as_view(), name="register"),
-
     path("play/start-session/", StartSession.as_view(), name="start-session"),
     path("play/get-session/", GetSession.as_view(), name="get-session"),
-
     path("play/sent-score/", SentScore.as_view(), name="sent-score"),
     path("play/list-score/", ListScore.as_view(), name="list-score"),
 ]
