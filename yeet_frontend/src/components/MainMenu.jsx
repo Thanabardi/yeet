c7038ec8c757b792141dd0a992d7f98ea71add4e
Kanpech <kankasem3002@gmail.com>
2022-02-17 17:36:06 +0700

    menu

--- yeet_frontend/src/components/MainMenu.jsx
+++ yeet_frontend/src/components/MainMenu.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
 import { Link } from 'react-router-dom';
-
+import '../assets/MainMeun.css'
 import '../assets/style.css';
 import AudioPlayer from './AudioPlayer'
 
@@ -15,19 +15,34 @@ const Menu = () => {
   // }
 
   return (
+    <div className='test'>
+      <h1 className='username'>{location.state}</h1>
     <div className='main-menu'>
-      <h1>Yeet</h1>
-      <h1>{location.state}</h1>
       {/* <img src="logo.PNG" alt="Yeet Logo"></img> */}
-
-      <Link to={'/play'} state={'Quick Play'}>Quick Play</Link>
-      <Link to={'/play'} state={'Competitive'}>Competitive</Link>
-      <Link to={'/rank'} state={'rank'}>Rank</Link>
-      <Link to={'/profile'} state={'Profile'}>Profile</Link>
-      <Link to={'/auth'} state={'Logout'}>Logout</Link>
+      <div className='Quick-Play'>
+      <Link to={'/play'} state={'Quick Play'}>QUICK PLAY</Link>
+      </div>
+      <div className='rank'>
+      <Link to={'/rank'} state={'rank'}>RANK</Link>
+      </div>
       {/* <button onClick={start}>Play</button> */}
-      <AudioPlayer audioPath={"/METATRON _SHIKI.mp3"}/>
       {/* <AudioPlayer audioPath={"https://actions.google.com/sounds/v1/water/waves_crashing_on_rock_beach.ogg"}/> */}
+    </div>
+    <div className='main-menu2'>
+    <div className='competitive'>
+      <Link to={'/play'} state={'Competitive'}>COMPETITIVE</Link>
+      </div>
+      </div>
+      <div className='main-menu'>
+      <div className='profile-menu'>
+      <Link to={'/profile'} state={'Profile'}>PROFILE</Link>
+     </div>
+      <div className='menu-logout'>
+      <Link to={'/auth'} state={'Logout'}>LOGOUT</Link>
+      </div>
+      <AudioPlayer audioPath={"/METATRON _SHIKI.mp3"}/>
+      
+    </div>
     </div>
   );
 }
